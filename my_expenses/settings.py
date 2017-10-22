c18d995566394e4808c0a6a6317f89615e6238d1
Steven <SeiryuZ@hotmail.com>
2017-10-22 17:09:22 +0700

    Skeleton for expenses and templates settings

--- my_expenses/settings.py
+++ my_expenses/settings.py
@@ -37,6 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+
+    'my_expenses.apps.expenses',
 ]
 
 MIDDLEWARE = [
@@ -54,7 +56,7 @@ ROOT_URLCONF = 'my_expenses.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [os.path.join(BASE_DIR, 'templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
--- my_expenses/urls.py
+++ my_expenses/urls.py
@@ -16,6 +16,9 @@ Including another URLconf
 from django.conf.urls import url
 from django.contrib import admin
 
+from .views import hello_world
+
 urlpatterns = [
+    # url(r'^hello-world/$', hello_world),
     url(r'^admin/', admin.site.urls),
 ]
